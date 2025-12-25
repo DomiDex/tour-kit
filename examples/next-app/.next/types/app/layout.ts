20a8f55090b130989c5de9aca3ff5aed473e2ed0
Dominique Degottex <75885483+DomiDex@users.noreply.github.com>
2025-12-25 09:53:36 +0700

    chore: update Next.js build and manifest files for improved performance and consistency, including changes to chunk names and routing configurations

--- examples/next-app/.next/types/app/layout.ts
+++ examples/next-app/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /mnt/c/Users/domi/Desktop/next-playground/tour-kit/examples/next-app/src/app/layout.tsx
+// File: C:\Users\domi\Desktop\next-playground\tour-kit\examples\next-app\src\app\layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- examples/next-app/.next/types/app/page.ts
+++ examples/next-app/.next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /mnt/c/Users/domi/Desktop/next-playground/tour-kit/examples/next-app/src/app/page.tsx
+// File: C:\Users\domi\Desktop\next-playground\tour-kit\examples\next-app\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
